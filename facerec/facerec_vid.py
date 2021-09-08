4552b5d3ad326ccea4007954d430604a299c32a7
mjshipp25 <mjshipp25@gmail.com>
2021-09-07 20:01:22 -0400

    First commit

---
+++ facerec/facerec_vid.py
@@ -0,0 +1,70 @@
+import face_recognition
+import os
+import cv2
+import pyttsx3
+
+KNOWN_FACES_DIR = "known_faces"
+# UKNOWN_FACES_DIR = "unknown_faces"
+TOLERANCE = 0.5
+FRAME_THICKNESS = 3
+FONT_THICKNESS = 2
+MODEL = "hog" # or hog for cpu only 
+
+video = cv2.VideoCapture(0)
+
+print("loading known faces")
+
+known_faces = []
+known_names = []
+
+for name in os.listdir(KNOWN_FACES_DIR):
+    for filename in os.listdir(f"{KNOWN_FACES_DIR}/{name}"):
+        image = face_recognition.load_image_file(f"{KNOWN_FACES_DIR}/{name}/{filename}")
+        encoding = face_recognition.face_encodings(image)
+        known_faces.append(encoding)
+        known_names.append(name)
+
+print("processing unknown faces")
+while True:
+    #print(filename, end='')
+
+    #image = face_recognition.load_image_file(f"{UNKNOWN_FACES_DIR}/{filename}")
+    ret, image = video.read()
+
+    locations = face_recognition.face_locations(image, model=MODEL)
+    encodings = face_recognition.face_encodings(image, locations)
+    # image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+    print(f', found {len(encodings)} face(s)')
+    for face_encoding, face_location in zip(encodings, locations):
+        for i in range(0,len(known_faces)):  #loops through each encoded known_face 
+            results = face_recognition.compare_faces(known_faces[i],face_encoding,TOLERANCE)
+
+        match = None
+        if True in results:
+            # match = file
+            print("Match found")
+
+            top_left = (face_location[3], face_location[0])
+            bottom_right = (face_location[1], face_location[2])
+
+            color = [0, 255, 0]
+
+            cv2.rectangle(image, top_left, bottom_right, color, FRAME_THICKNESS)
+
+            top_left = (face_location[3], face_location[2])
+            bottom_right = (face_location[1], face_location[2]+22)
+
+            cv2.rectangle(image, top_left, bottom_right, color, cv2.FILLED)
+            cv2.putText(image, "Max", (face_location[3]+10, face_location[2]+15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (200, 200, 200), FONT_THICKNESS)
+
+        if False in results:
+            engine = pyttsx3.init()
+            engine.say("intruder alert")
+            engine.runAndWait()
+
+    cv2.imshow(filename, image)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+    # cv2.waitKey(10000)
+    # cv2.destroyWindow(filename)
